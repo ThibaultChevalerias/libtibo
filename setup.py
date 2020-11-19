225732fa88c5765fc35c924af1a00779d645cb80
Thibault CHEVALERIAS <thibault.chevalerias@arcelormittal.com>
2020-11-19 15:59:08 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='libtibo',
-    version='0.1.0',
+    version='0.1.1',
     description='Personal test package',
     url='https://github.com/ThibaultChevalerias/libtibo',
     author='Thibault Chevalerias',
