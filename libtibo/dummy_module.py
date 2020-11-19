3c15303a2f41a795c8c7c88b65e2eda2f777b8ee
Thibault CHEVALERIAS <thibault.chevalerias@arcelormittal.com>
2020-11-19 15:31:55 +0100

    Add sample code and tests to be used with pytest

---
+++ libtibo/dummy_module.py
@@ -0,0 +1,9 @@
+"""
+    Dummy module for testing pytest
+"""
+
+def flip_list(l):
+    return l[::-1]
+
+def get_hello_str(name):
+    return 'Hello {}!'.format(name)
---
+++ tests/test_dummy_module.py
@@ -0,0 +1,24 @@
+from libtibo.dummy_module import flip_list, get_hello_str
+import numpy as np
+
+def test_flip_list_empty():
+    assert flip_list([]) == []
+
+def test_flip_list_one_element():
+    assert flip_list([0]) == [0]
+    assert flip_list(['0']) == ['0']
+
+def test_flip_list_numpy_array():
+    assert np.array_equal(
+        flip_list(np.array([1, 2, 3])),
+        np.array([3, 2, 1])
+        )
+
+def test_get_hello_str_empty():
+    assert get_hello_str('') == 'Hello !'
+
+def test_get_hello_str_float():
+    assert get_hello_str(3.14) == 'Hello 3.14!'
+
+def test_get_hello_str_regular():
+    assert get_hello_str('Alice') == 'Hello Alice!'
